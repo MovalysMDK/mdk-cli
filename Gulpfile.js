5dd1a6ff4f10e444c60558e220a0ee7d773032a9
lmichenaud <laurent.michenaud@sopra.com>
2015-10-01 14:48:40 +0200

    update configuration + update add platform

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var jsonlint = require("gulp-jsonlint");
+var jsdoc = require("gulp-jsdoc");
 
 gulp.task("lint", function() {
     return gulp.src("lib/**/*.js")
@@ -11,8 +12,13 @@ gulp.task("lint", function() {
 gulp.task("jsonlint", function() {
     return gulp.src("./lib/**/*.json")
         .pipe(jsonlint())
-         .pipe(jsonlint.reporter());
+        .pipe(jsonlint.reporter());
 });
 
-gulp.task('default', ['lint','jsonlint']);
+gulp.task("jsdoc", function() {
+    return gulp.src("./lib/**/*.js")
+	.pipe(jsdoc('./api'));
+});
+
+gulp.task('default', ['lint','jsonlint','jsdoc']);
 
