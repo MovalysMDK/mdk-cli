aa4d0ca39d0e68d4e5a1465bcbd485fa65889aa5
lmichenaud <laurent.michenaud@sopra.com>
2015-10-08 11:48:50 +0200

    add android sdk installation for osx + add platform + build platform

--- lib/devtools/install/android/installSdk.template.sh
+++ lib/devtools/install/android/installSdk.template.sh
@@ -6,3 +6,13 @@ expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
+
+{{#cmdBuildTools}}
+set timeout -1;
+spawn {{{cmdBuildTools}}};
+expect {
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+{{/cmdBuildTools}}
+
